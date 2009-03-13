5532758e66d180c5b6eab1998028f347cf2d0b59
zhen.sydow <zhen.sydow@5a5b6589-463f-0410-af11-d30a242298a8>
2009-03-13 10:01:16 +0000

    Ejemplo de codigo Fortran

---
+++ fortran/helloworld.f90
@@ -0,0 +1,10 @@
+!-------------------------------------------------------------------------------
+! Hello World in Fortran
+! by Luis Jose Cabellos
+!-------------------------------------------------------------------------------
+PROGRAM HelloWorld
+  IMPLICIT NONE
+
+  WRITE(*,*) "Hello World"
+
+END PROGRAM HelloWorld
