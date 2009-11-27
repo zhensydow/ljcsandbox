1bb2f95f244472cb7374191f98aa9ba8a759ac29
zhen.sydow <zhen.sydow@5a5b6589-463f-0410-af11-d30a242298a8>
2009-11-27 15:26:10 +0000

    xmonad en ubuntu 9.10

--- programas/xmonad_02/xmonad.hs
+++ programas/xmonad_02/xmonad.hs
@@ -1,56 +1,87 @@
-import XMonad
-import qualified XMonad.StackSet as W( focusDown, focusUp
-                                     , greedyView )
-import XMonad.Layout.Tabbed
+import XMonad( terminal, workspaces, manageHook, (<+>), logHook, layoutHook, spawn, shiftMask, windows
+             , keys, modMask, controlMask, mod4Mask, XConfig(..), (.|.), sendMessage
+             , xK_a, xK_b, xK_d, xK_e, xK_f, xK_g, xK_i, xK_l, xK_m, xK_n, xK_p, xK_s, xK_w, xK_y
+             , xK_Up, xK_Down, xK_Left, xK_Right, xK_Tab
+             , borderWidth, normalBorderColor, focusedBorderColor, focusFollowsMouse
+             , Full(..), Mirror(..), Tall(..), (|||) )
+import qualified XMonad.StackSet as W( focusDown, focusUp, greedyView )
+import XMonad.Main( xmonad )
+import XMonad.Config( defaultConfig )
+import XMonad.ManageHook( resource, doIgnore, (=?) )
+import XMonad.Util.Run( spawnPipe )
+import XMonad.Hooks.ManageDocks( manageDocks, avoidStruts, ToggleStruts(..) )
+import XMonad.Hooks.ManageHelpers( composeOne, (-?>) )
+import XMonad.Hooks.DynamicLog( dynamicLogWithPP, PP(..), xmobarColor, xmobarPP, wrap, shorten )
+import XMonad.Actions.CycleWS( prevWS, nextWS, shiftToPrev, shiftToNext, toggleWS )
 import XMonad.Actions.Submap( submap )
-import XMonad.Actions.Search as Search
-import XMonad.Actions.CycleWS( prevWS, nextWS
-                             , shiftToPrev, shiftToNext, toggleWS )
-import XMonad.Layout.NoBorders( noBorders, smartBorders )
+import XMonad.Layout.Gaps( GapMessage(..), gaps, Direction2D(..) )
+import XMonad.Layout.ShowWName( showWName', defaultSWNConfig, SWNConfig(..) )
 import XMonad.Layout.PerWorkspace( onWorkspace )
-import XMonad.Layout.ShowWName
-import XMonad.Layout.LayoutCombinators((*/*))
-import XMonad.Layout.Gaps 
-import XMonad.Hooks.DynamicLog
-import XMonad.Hooks.ManageDocks( manageDocks, avoidStruts
-                               , ToggleStruts(..) )
+import XMonad.Layout.NoBorders( noBorders, smartBorders )
+import XMonad.Layout.Tabbed( tabbed, defaultTheme, shrinkText, Theme(..) )
 import XMonad.Prompt( defaultXPConfig )
 import XMonad.Prompt.Ssh( sshPrompt )
-import XMonad.Util.Run( spawnPipe )
+import XMonad.Actions.Search as Search( promptSearch, selectSearch
+                                      , imdb, google, wikipedia, maps, mathworld, youtube )
+import XMonad.Layout.LayoutCombinators((*/*))
+
 import System.IO( hPutStrLn )
 import qualified Data.Map as M( union, fromList )
 
 main = do
   xmproc <- spawnPipe "/usr/local/bin/xmobar /home/cabellos/.xmonad/xmobar"
   xmonad $ defaultConfig
-             { terminal = "urxvt"
-             , manageHook = manageDocks <+> manageHook defaultConfig
-             , layoutHook = showWName' mySWNConfig myLayout
+       { terminal = "urxvt" 
+       , manageHook = (composeOne [ resource =? "stalonetray" -?> doIgnore ]) <+> manageDocks <+> manageHook defaultConfig
+       , layoutHook = showWName' mySWNConfig myLayout
+       , workspaces = myWorkspaces
 
-             -- xmobar configuration
-             , logHook = dynamicLogWithPP $ xmobarPP 
-                         { ppOutput = hPutStrLn xmproc . wrap "< " " >"
-                         , ppCurrent = 
-                             xmobarColor "yellow" "" . (wrap "[" "]" ) . (drop 2)
-                         , ppVisible = (\_->"")
-                         , ppHidden = xmobarColor "red" "" . (take 1)
-                         , ppHiddenNoWindows = take 1
-                         , ppLayout = (\_->"")
-                         , ppUrgent = (\_->"")
-                         , ppSep = " >-< "
-                         , ppTitle = xmobarColor "green" "" . shorten 50 }
-             , workspaces = myWorkspaces
+       -- xmobar configuration
+       , logHook = dynamicLogWithPP $ xmobarPP 
+                   { ppOutput = hPutStrLn xmproc . wrap "< " " >"
+                   , ppCurrent = xmobarColor "yellow" "" . (wrap "[" "]" ) . (drop 2)
+                   , ppVisible = (\_->"")
+                   , ppHidden = xmobarColor "red" "" . (take 1)
+                   , ppHiddenNoWindows = take 1
+                   , ppLayout = (\_->"")
+                   , ppUrgent = (\_->"")
+                   , ppSep = " >-< "
+                   , ppTitle = xmobarColor "green" "" . shorten 50 }
 
-             -- Key configuration 
-             , modMask = mod4Mask
-             , keys = \c -> myKeys c `M.union` keys defaultConfig c
+       -- Key configuration 
+       , modMask = mod4Mask
+       , keys = \c -> myKeys c `M.union` keys defaultConfig c
 
-             -- xmonad style
-             , borderWidth = 1
-             , normalBorderColor = "#aa99aa"
-             , focusedBorderColor = "#10aa10"
-             , focusFollowsMouse = False
-             }
+       -- xmonad style
+       , borderWidth = 1
+       , normalBorderColor = "#aa99aa"
+       , focusedBorderColor = "#10aa10"
+       , focusFollowsMouse = False
+       }
+
+web = "1:web"
+media = "2:media"
+pidgin = "6:pidgin"
+
+myWorkspaces = [ web,media,"3:code1","4:code2","5:apps",pidgin ]
+
+myLayout = onWorkspace web (gaps [(U,16)] $ avoidStruts $ smartBorders myTabbed) $
+           onWorkspace media (noBorders Full) $
+           onWorkspace pidgin (gaps [(U,16)] $ avoidStruts $ (fitTiled ||| myTabbed)) $
+           gaps [(U,16)] $ avoidStruts $ smartBorders $ (normalTiled ||| Mirror normalTiled ||| myTabbed)
+    where
+      normalTiled = Tall 1 (2/100) (1/2)
+      fitTiled = Tall 1 (2/100) (1/4)
+      myTabbed = tabbed shrinkText myTabConfig
+
+myTabConfig = defaultTheme 
+              { activeColor = "#007046"
+              , activeTextColor = "#FFFFFF"
+              , activeBorderColor = "#20815D"
+              , inactiveColor = "#0C0874"
+              , inactiveTextColor = "#AAAAAA"
+              , inactiveBorderColor = "#333086"
+              , decoHeight = 12 }
 
 mySWNConfig = defaultSWNConfig
               { swn_font    = "-misc-fixed-*-*-*-*-12-*-*-*-*-*-*-*"
@@ -70,8 +101,9 @@ myKeys conf@(XConfig {modMask = modm}) =
          , ((modm, xK_a), spawn "urxvt -e aumix" )
          -- Full Screen
          , ((modm, xK_b), do
-              sendMessage ToggleStruts 
-              sendMessage ToggleGaps )
+               sendMessage ToggleStruts 
+               sendMessage ToggleGaps )
+
          -- Configuracion para navegar entre ventanas
          , ((modm, xK_Left), prevWS ) 
          , ((modm, xK_Right), nextWS )
@@ -83,8 +115,7 @@ myKeys conf@(XConfig {modMask = modm}) =
          -- Busquedas
          , ((modm, xK_s), submap $ searchMap $ Search.promptSearch defaultXPConfig) 
          , ((modm .|. shiftMask, xK_s), submap $ searchMap $ Search.selectSearch) 
-         -- Salir
-         , ((modm .|. shiftMask, xK_F12), spawn "/usr/bin/gnome-power-cmd suspend") ]
+         ]
 
 internetMap = M.fromList $
               [ ((0, xK_f), spawn "chromium-browser --enable-plugins" )
@@ -102,28 +133,3 @@ searchMap method = M.fromList $
                    , ((0, xK_m), mySearch $ method Search.maps ) 
                    , ((0, xK_d), mySearch $ method Search.mathworld )
                    , ((0, xK_y), mySearch $ method Search.youtube ) ]
-
-web = "1:web"
-media = "2:multimedia"
-pidgin = "6:pidgin"
-
-myWorkspaces = [ web,media,"3:code01","4:code02","5:apps",pidgin ]
-
---myLayout = onWorkspace web (gaps [(L,100)] $ avoidStruts $ smartBorders myTabbed) $
-myLayout = onWorkspace web (avoidStruts $ smartBorders myTabbed) $
-           onWorkspace media (noBorders Full) $
-           onWorkspace pidgin (avoidStruts $ (fitTiled ||| myTabbed)) $
-           avoidStruts $ smartBorders $ (normalTiled ||| Mirror normalTiled ||| myTabbed)
-    where
-      normalTiled = Tall 1 (2/100) (1/2)
-      fitTiled = Tall 1 (2/100) (1/4)
-      myTabbed = tabbed shrinkText myTabConfig
-
-myTabConfig = defaultTheme 
-              { activeColor = "#007046"
-              , activeTextColor = "#FFFFFF"
-              , activeBorderColor = "#20815D"
-              , inactiveColor = "#0C0874"
-              , inactiveTextColor = "#AAAAAA"
-              , inactiveBorderColor = "#333086"
-              , decoHeight = 12 }
