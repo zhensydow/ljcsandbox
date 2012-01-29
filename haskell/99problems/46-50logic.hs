40f9b007a4806ebd5e02d83b5013bfc2b7296693
zhen.sydow <zhen.sydow@5a5b6589-463f-0410-af11-d30a242298a8>
2012-01-29 19:00:39 +0000

    all logic problems

--- haskell/99problems/46-50logic.hs
+++ haskell/99problems/46-50logic.hs
@@ -1,4 +1,5 @@
-import Data.List( permutations )
+import Data.List( permutations, sortBy )
+import Data.Ord( comparing )
 import Control.Monad( forM_ )
 
 combinations :: Eq a => Int -> [a] -> [[a]]
@@ -72,11 +73,15 @@ False False False
 -}
 infixl 6 `or'`
 infixl 7 `and'`
+infixl 3 `equ'`
 
 {-| Problem 48
 (**) Truth tables for logical expressions (3).
 
-Generalize problem P47 in such a way that the logical expression may contain any number of logical variables. Define table/2 in a way that table(List,Expr) prints the truth table for the expression Expr, which contains the logical variables enumerated in List.
+Generalize problem P47 in such a way that the logical expression may contain any
+number of logical variables. Define table/2 in a way that table(List,Expr)
+prints the truth table for the expression Expr, which contains the logical
+variables enumerated in List.
 
 Example:
 
@@ -111,37 +116,76 @@ False True  True  False
 False True  False False
 False False True  False
 False False False False
-Solutions
+-}
+gen :: Int -> [a] -> [[a]]
+gen 1 xs = map (\x -> [x]) xs
+gen n xs = [x:ys | x <- xs, ys <- gen (n-1) xs]
 
-5 Problem 49
+tablen n ff = do
+  forM_ (gen n [True,False]) $ \xs -> do
+    forM_ xs $ \x -> do      
+      putStr $ show x ++ " "
+    putStrLn $ show $ ff xs
+
+{-| Problem 49
 (**) Gray codes.
 
-An n-bit Gray code is a sequence of n-bit strings constructed according to certain rules. For example,
+An n-bit Gray code is a sequence of n-bit strings constructed according to
+certain rules. For example,
 
 n = 1: C(1) = ['0','1'].
 n = 2: C(2) = ['00','01','11','10'].
 n = 3: C(3) = ['000','001','011','010',´110´,´111´,´101´,´100´].
-Find out the construction rules and write a predicate with the following specification:
+
+Find out the construction rules and write a predicate with the following
+specification:
 
 % gray(N,C) :- C is the N-bit Gray code
-Can you apply the method of "result caching" in order to make the predicate more efficient, when it is to be used repeatedly?
+
+Can you apply the method of "result caching" in order to make the predicate more
+efficient, when it is to be used repeatedly?
 
 Example in Haskell:
 
 P49> gray 3
 ["000","001","011","010","110","111","101","100"]
-Solutions
+-}
+gray :: Int -> [String]
+gray 1 = ["0", "1"]
+gray n = (map ('0':) xs) ++ (map ('1':) $ reverse xs)
+  where xs = gray (n-1)
 
-
-6 Problem 50
+{-| Problem 50
 (***) Huffman codes.
 
-We suppose a set of symbols with their frequencies, given as a list of fr(S,F) terms. Example: [fr(a,45),fr(b,13),fr(c,12),fr(d,16),fr(e,9),fr(f,5)]. Our objective is to construct a list hc(S,C) terms, where C is the Huffman code word for the symbol S. In our example, the result could be Hs = [hc(a,'0'), hc(b,'101'), hc(c,'100'), hc(d,'111'), hc(e,'1101'), hc(f,'1100')] [hc(a,'01'),...etc.]. The task shall be performed by the predicate huffman/2 defined as follows:
+We suppose a set of symbols with their frequencies, given as a list of fr(S,F)
+terms. Example: [fr(a,45),fr(b,13),fr(c,12),fr(d,16),fr(e,9),fr(f,5)]. Our
+objective is to construct a list hc(S,C) terms, where C is the Huffman code word
+for the symbol S. In our example, the result could be Hs = [hc(a,'0'),
+hc(b,'101'), hc(c,'100'), hc(d,'111'), hc(e,'1101'), hc(f,'1100')]
+[hc(a,'01'),...etc.]. The task shall be performed by the predicate huffman/2
+defined as follows:
 
 % huffman(Fs,Hs) :- Hs is the Huffman code table for the frequency table Fs
 Example in Haskell:
 
 *Exercises> huffman [('a',45),('b',13),('c',12),('d',16),('e',9),('f',5)]
 [('a',"0"),('b',"101"),('c',"100"),('d',"111"),('e',"1101"),('f',"1100")]
-Solutions
 -}
+data HuffmanTree = TreeElem (Char,Int)
+                 | TreeNode Int HuffmanTree HuffmanTree
+                   deriving( Show )
+                           
+huffman :: [(Char,Int)] -> [(Char,String)]
+huffman xs = sortBy (comparing fst) . fff "" 
+             . mkTree . map TreeElem . sortBy (comparing snd) $ xs
+
+mkTree [x] = x
+mkTree (x:y:xs) = mkTree . sortBy (comparing freq) 
+                  $ (TreeNode (freq x + freq y) x y : xs)
+  where
+    freq (TreeElem (_,a)) = a
+    freq (TreeNode a _ _) = a
+    
+fff xs (TreeElem (a,_)) = [(a,xs)]
+fff xs (TreeNode _ x y) = (fff (xs ++ "0") x) ++ (fff (xs ++ "1") y)
