4efe91929c835368dde5ff78f37b2e83267ef77c
Luis Cabellos <zhen.sydow@gmail.com>
2013-02-11 11:17:10 +0100

    add file exist check

--- c++/opengl/fileutils.cpp
+++ c++/opengl/fileutils.cpp
@@ -3,17 +3,23 @@
 
 //------------------------------------------------------------------------------
 #include <fstream>
+#include <iostream>
 
 //------------------------------------------------------------------------------
 void readFileData( const std::string &str, std::string &data ){
-  std::ifstream t( str );
+    std::ifstream t( str );
 
-  t.seekg( 0, std::ios::end );
-  data.reserve( t.tellg() );
-  t.seekg( 0, std::ios::beg );
+    if( t.good() ){
+        t.seekg( 0, std::ios::end );
+        data.reserve( t.tellg() );
+        t.seekg( 0, std::ios::beg );
 
-  data.assign( std::istreambuf_iterator<char>(t),
-	       std::istreambuf_iterator<char>());
+        data.assign( std::istreambuf_iterator<char>(t),
+                     std::istreambuf_iterator<char>());
+    }else{
+        std::cerr << "File '" << str << "' doesn't exist\n";
+        std::terminate();
+    }
 }
 
 //------------------------------------------------------------------------------
