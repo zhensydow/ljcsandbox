97a1e5400dda02c724ab05d63097723692ca0df1
Luis Cabellos <zhen.sydow@gmail.com>
2014-11-19 13:01:57 +0100

    solved Problem 4

--- edx/m6002x/ProblemSet4/ps4.py
+++ edx/m6002x/ProblemSet4/ps4.py
@@ -7,7 +7,7 @@ from ps3b import *
 
 #
 # PROBLEM 1
-#        
+#
 def simulationDelayedTreatment(numTrials):
     """
     Runs simulations and make histograms for problem 1.
@@ -21,10 +21,64 @@ def simulationDelayedTreatment(numTrials):
 
     numTrials: number of simulation runs to execute (an integer)
     """
-    
-    # TODO
 
+    numViruses = 100
+    maxBirthProb = 0.1
+    clearProb = 0.05
+    resistances = {'guttagonol':True}
+    mutProb = 0.005
+    maxPop = 1000
+    delayTime = 150
 
+    for k,delayTime in enumerate([0,75,150,300]):
+        print "Doing ", delayTime
+        maxTime = delayTime + 150
+        virusPop = [0]*maxTime
+        virusFinalPop = [0]*numTrials
+        numCured = 0
+
+        for j in xrange(numTrials):
+            viruses = []
+            for i in xrange(numViruses):
+                viruses.append( ResistantVirus( maxBirthProb, clearProb,
+                                                resistances, mutProb ) )
+
+            patient = TreatedPatient( viruses, maxPop )
+
+            for i in xrange(delayTime):
+                virusPop[i] = virusPop[i] + patient.update()
+
+            patient.addPrescription( 'guttagonol' )
+
+            for i in xrange(150):
+                virusPop[i+delayTime] = virusPop[i+delayTime] + patient.update()
+
+            virusFinalPop[j] = patient.getTotalPop()
+            if virusFinalPop[j] <=50:
+                numCured = numCured + 1
+
+        print "Cured ", numCured / float(numTrials)
+
+        for i in range(maxTime):
+            virusPop[i] = virusPop[i] / float(numTrials)
+
+        pylab.figure(1)
+        pylab.subplot( 2, 2, k+1 )
+        pylab.hist( virusFinalPop, 50, label='Virus Pop' )
+        pylab.title( 'Delay time '+str(delayTime) )
+        pylab.legend()
+        v = pylab.axis()
+        pylab.axis( [0,150+300,v[2],v[3]] )
+
+        pylab.figure(2)
+        pylab.subplot( 2, 2, k+1 )
+        pylab.plot( virusPop, label='Virus Pop' )
+        pylab.title( 'Delay time '+str(delayTime) )
+        pylab.legend()
+        v = pylab.axis()
+        pylab.axis( [0,150+300,v[2],v[3]] )
+
+    pylab.show()
 
 
 
@@ -45,4 +99,69 @@ def simulationTwoDrugsDelayedTreatment(numTrials):
 
     numTrials: number of simulation runs to execute (an integer)
     """
-    # TODO
+    numViruses = 100
+    maxBirthProb = 0.1
+    clearProb = 0.05
+    resistances = {'guttagonol':False,'grimpex':False}
+    mutProb = 0.005
+    maxPop = 1000
+    delayTime = 150
+
+    for k,delayTime in enumerate([0,75,150,300]):
+        print "Doing ", delayTime
+
+        maxTime = 150 + delayTime + 150
+        virusPop = [0]*maxTime
+        virusFinalPop = [0]*numTrials
+        numCured = 0
+
+        for j in xrange(numTrials):
+            viruses = []
+            for i in xrange(numViruses):
+                viruses.append( ResistantVirus( maxBirthProb, clearProb,
+                                                resistances, mutProb ) )
+
+            patient = TreatedPatient( viruses, maxPop )
+
+            for i in xrange(150):
+                virusPop[i] = virusPop[i] + patient.update()
+
+            patient.addPrescription( 'guttagonol' )
+
+            ofs = 150
+            for i in xrange(delayTime):
+                virusPop[i+ofs] = virusPop[i+ofs] + patient.update()
+
+            patient.addPrescription( 'grimpex' )
+
+            ofs = 150 + delayTime
+            for i in xrange(150):
+                virusPop[i+ofs] = virusPop[i+ofs] + patient.update()
+
+            virusFinalPop[j] = patient.getTotalPop()
+            if virusFinalPop[j] <=50:
+                numCured = numCured + 1
+
+        print "Cured ", numCured / float(numTrials)
+
+        for i in range(maxTime):
+            virusPop[i] = virusPop[i] / float(numTrials)
+
+        pylab.figure(1)
+        pylab.subplot( 2, 2, k+1 )
+        pylab.hist( virusFinalPop, 50, label='Virus Pop' )
+        pylab.title( 'Delay time '+str(delayTime) )
+        pylab.legend()
+        #v = pylab.axis()
+        #pylab.axis( [0,150+300,v[2],v[3]] )
+
+        pylab.figure(2)
+        pylab.subplot( 2, 2, k+1 )
+        pylab.plot( virusPop, label='Virus Pop' )
+        pylab.title( 'Delay time '+str(delayTime) )
+        pylab.legend()
+        #v = pylab.axis()
+        #pylab.axis( [0,150+300,v[2],v[3]] )
+
+    pylab.show()
+
