ea87e05aa7aa3351aed322f6d7c508a2c68a984b
Luis Cabellos <zhen.sydow@gmail.com>
2015-04-09 16:06:58 +0200

    solve p3 / q3

--- edx/cs1881x/p3/reinforcement/analysis.py
+++ edx/cs1881x/p3/reinforcement/analysis.py
@@ -25,41 +25,38 @@ def question2():
     answerNoise = 0.0
     return answerDiscount, answerNoise
 
-def question3a():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+#---------------------------------------------------------------------
+def question3a(): #pass
+    answerDiscount = 0.9
+    answerNoise = 0.2
+    answerLivingReward = -2
     return answerDiscount, answerNoise, answerLivingReward
-    # If not possible, return 'NOT POSSIBLE'
 
 def question3b():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+    answerDiscount = 0.5
+    answerNoise = 0.4
+    answerLivingReward = -1.5
     return answerDiscount, answerNoise, answerLivingReward
-    # If not possible, return 'NOT POSSIBLE'
 
-def question3c():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+def question3c(): #pass
+    answerDiscount = 0.9
+    answerNoise = 0.0
+    answerLivingReward = 0
     return answerDiscount, answerNoise, answerLivingReward
-    # If not possible, return 'NOT POSSIBLE'
 
-def question3d():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+def question3d(): #pass
+    answerDiscount = 0.9
+    answerNoise = 0.5
+    answerLivingReward = 0
     return answerDiscount, answerNoise, answerLivingReward
-    # If not possible, return 'NOT POSSIBLE'
 
-def question3e():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+def question3e(): #pass
+    answerDiscount = 0.9
+    answerNoise = 0.0
+    answerLivingReward = 2
     return answerDiscount, answerNoise, answerLivingReward
-    # If not possible, return 'NOT POSSIBLE'
 
+#---------------------------------------------------------------------
 def question6():
     answerEpsilon = None
     answerLearningRate = None
